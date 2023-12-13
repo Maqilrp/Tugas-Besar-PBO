096c4a0d4ad7a9036afbf0bd4e23007ebe180534
martinrahmanhakim <martinrahmanhakim@student.telkomuniversity.ac.id>
2023-12-13 15:36:41 +0700

    draft final untuk tubes

--- src/AdminPage/AdminFasilitas.java
+++ src/AdminPage/AdminFasilitas.java
@@ -8,12 +8,12 @@ package AdminPage;
  *
  * @author AqilR
  */
-public class AdminFasilitasView extends javax.swing.JFrame {
+public class AdminFasilitas extends javax.swing.JFrame {
 
     /**
      * Creates new form adminFasilitas2
      */
-    public AdminFasilitasView() {
+    public AdminFasilitas() {
         initComponents();
     }
 
@@ -351,14 +351,18 @@ public class AdminFasilitasView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminFasilitasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminFasilitas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminFasilitasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminFasilitas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminFasilitasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminFasilitas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminFasilitasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminFasilitas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -367,7 +371,7 @@ public class AdminFasilitasView extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdminFasilitasView().setVisible(true);
+                new AdminFasilitas().setVisible(true);
             }
         });
     }
--- src/AdminPage/AdminTamu.java
+++ src/AdminPage/AdminTamu.java
@@ -8,12 +8,12 @@ package AdminPage;
  *
  * @author AqilR
  */
-public class AdminTamuView extends javax.swing.JFrame {
+public class AdminTamu extends javax.swing.JFrame {
 
     /**
      * Creates new form AdminTamuView
      */
-    public AdminTamuView() {
+    public AdminTamu() {
         initComponents();
     }
 
@@ -94,20 +94,21 @@ public class AdminTamuView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminTamuView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminTamu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminTamuView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminTamu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminTamuView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminTamu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminTamuView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminTamu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdminTamuView().setVisible(true);
+                new AdminTamu().setVisible(true);
             }
         });
     }
